14ddb228d450add0f6c6bbf66ef0e62a16b22e26
didadadida93 <didadadida93@gmail.com>
2020-09-16 19:37:22 +0700

    update app so now it can serve react app

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 /* const job = require('./helpers/cron-job') */
+const path = require("path")
 const express = require("express")
 const app = express()
 
@@ -19,6 +20,14 @@ if (process.env.NODE_ENV == "development") app.use(require("cors")())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static("public"))
-app.use("/", require("./routes"))
+app.use("/api", require("./routes/api"))
+
+
+function defaultHandler(req, res) {
+  res.sendFile(path.join(__dirname), "public", "index.html")
+}
+
+app.get("/", defaultHandler)
+app.get("/*", defaultHandler)
 
 module.exports = app
