30720584c62aa371e04ef72caa665ab238bc77af
didadadida93 <didadadida93@gmail.com>
2020-09-30 07:46:07 +0700

    Task now use `Promise.all`
    it will wait till all promise get resolved, then continue the
    application flow.

--- helpers/cron-job.js
+++ helpers/cron-job.js
@@ -1,23 +1,51 @@
+const { player: Player, population: Population } = require("../models").models
 const { CronJob } = require("cron")
-const { getState } = require("../store")
-const httpClient = require("./http-client")
+const requestMapData = require("./request-map-data")
+
+function addData({ name, tribeId, kingdomId, tkPlayerId, population }) {
+  return new Promise((resolve, reject) => {
+    Player.findCreateFind({
+      where: { tkPlayerId },
+      defaults: { name, tribeId, kingdomId, tkPlayerId }
+    })
+      .then(([ player ]) => Population.create({ playerId: player.id, population }))
+      .then(resolve)
+      .catch(reject)
+  })
+}
 
 async function task() {
-  // it should have error handler
-  try {
-    let { gameworldSession, cookies } = getState()
+  let notDone = true
 
-    let data = {
-      action: "",
-      controller: "",
-      params: {},
-      session: gameworldSession
+  while (notDone) {
+    try {
+      let data = await requestMapData()
+      // it should check the response, if there is any error idk what todo
+
+      let cells = Object.keys(data.response["1"].region)
+        .reduce((a, id) => [...a, ...data.response["1"].region[id]], [])
+        .filter(cell => cell.playerId != undefined)
+
+      let promises = Object.keys(data.response["1"].player)
+        .map(tkPlayerId => {
+          let { name, tribeId, kingdomId } = data.response["1"].player[tkPlayerId]
+          let population = cells
+            .filter(cell => cell.playerId == tkPlayerId)
+            .reduce((a, cell) => +cell.village.population + a, 0)
+
+          return addData({ name, tribeId, kingdomId, tkPlayerId, population })
+        })
+
+      await Promise.all(promises)
+
+      notDone = false
+    } catch (e) {
+      console.log(e)
+      console.log("Error happening")
+
+      // for now it will not try to sent request again
+      notDone = false
     }
-
-    console.log("Task running at", new Date())
-  } catch (e) {
-    console.log(e)
-    console.log("Error happening")
   }
 }
 
