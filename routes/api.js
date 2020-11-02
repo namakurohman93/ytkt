770fc41f95f25ca2c7beacabed8f2eb533248e36
didadadida93 <didadadida93@gmail.com>
2020-11-03 06:32:48 +0700

    Add new route for finding inactive

--- routes/api.js
+++ routes/api.js
@@ -5,5 +5,6 @@ router.get("/status", ApiController.getStatus)
 router.post("/login", ApiController.loginHandler)
 router.get("/players", ApiController.getAllPlayers)
 router.get("/players/:playerId", ApiController.getPlayer)
+router.get("/inactive", ApiController.getInactive)
 
 module.exports = router
